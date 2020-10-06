3e88cf94cb355941334eaad4e85b77f8c2ad5bb7
shane114 <72244493+shane114@users.noreply.github.com>
2020-10-06 10:51:16 +0800

    Update __manifest__.py

--- openacademy/__manifest__.py
+++ openacademy/__manifest__.py
@@ -1,24 +1,23 @@
 # -*- coding: utf-8 -*-
 {
-    'name': "openacademy",
+    'name': "Open Academy",
 
-    'summary': """
-        Open Academy module for managing trainings:
-            - training courses
-            -training sessions
-            -attendees registration""",
+    'summary': """Manage trainings""",
 
     'description': """
-        Long description of module's purpose
+        Open Academy module for managing trainings:
+            - training courses
+            - training sessions
+            - attendees registration
     """,
 
-    'author': "Toolkit",
+    'author': "My Company",
     'website': "http://www.yourcompany.com",
 
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/13.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
-    'category': 'Uncategorized',
+    'category': 'Test',
     'version': '0.1',
 
     # any module necessary for this one to work correctly
@@ -27,12 +26,10 @@
     # always loaded
     'data': [
         # 'security/ir.model.access.csv',
-        'views/views.xml',
-        'views/templates.xml',
-        'views/openacademy.xml'
+        'templates.xml',
     ],
     # only loaded in demonstration mode
     'demo': [
-        'demo/demo.xml',
+        'demo.xml',
     ],
 }
