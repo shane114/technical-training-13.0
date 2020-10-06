8a30c3de435afac4fb56943c4ae5ec9bf3c8d475
shane114 <72244493+shane114@users.noreply.github.com>
2020-10-06 10:51:26 +0800

    Update models.py

--- openacademy/models/models.py
+++ openacademy/models/models.py
@@ -1,8 +1,8 @@
 from odoo import models, fields, api
 
-class Course(models.Model):
-    _name = 'openacademy.course'
-    _description = "OpenAcademy Courses"
+# class Course(models.Model):
+#     _name = 'openacademy.course'
+#     _description = "OpenAcademy Courses"
 
-    name = fields.Char(string="Title", required=True)
-    description = fields.Text()
+#     name = fields.Char(string="Title", required=True)
+#     description = fields.Text()
